f26b4432a992f2a83954fe39069e4020c7022f17
MAYUBXG <medaymenhmila@gmail.com>
2022-09-05 13:34:25 +0100

    second commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Form } from 'react-bootstrap';
 
 
 
-function BasicExample(props) {
+
+function BasicExample() {
   return (
     <div>
-    <Navbar bg="light" expand="lg">
+    <Navbar bg="dark" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="/">Medi@Land</Navbar.Brand>
+        <Navbar.Brand href="/"style={{color:"white"}}>Medi@Land</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -18,20 +18,14 @@ function BasicExample(props) {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
+            
            
             
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              className='form-control'
-              value={props.value}
-              onChange={(event) => props.setSearchValue(event.target.value)}
-              placeholder='Type to search...'
-            />
-            
-          </Form>
+          
+          
         </Navbar.Collapse>
+        
       </Container>
     </Navbar>
     
